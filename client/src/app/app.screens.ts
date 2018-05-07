69f0f6a0851be7bbdae3c66be404bd95971a0af2
Selva P <selvabharathi.p@flex.com>
2018-05-07 02:35:30 -0700

    [Studio] Delete screen "newhome"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,15 +1,9 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu';
-import { newhome_PhonePortrait } from '../pages/newhome/PhonePortrait/newhome';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent,
-    newhome_PhonePortrait
+    MenuComponent
   ];
-  static mapping = {
-    'newhome': {
-      PhonePortrait: newhome_PhonePortrait
-    }
-  }
+  static mapping = {}
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,2 +1,1 @@
 export * from './menu/index';
-export * from './newhome/index';
