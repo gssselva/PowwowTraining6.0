566356cb0e926db10f16201ffc5feb613d822f26
Selva P <selvabharathi.p@flex.com>
2018-05-07 04:25:05 -0700

    [Webexplorer] Delete connection from page model "homeiconcolorblocks", Connect action to page model "homeiconcolorblocks", Update match in connection of page model "homeiconcolorblocks"

--- server/transform/powwowtraining60/homeiconcolorblocks.js
+++ server/transform/powwowtraining60/homeiconcolorblocks.js
@@ -2,3 +2,6 @@ exports.homeiconcolorblocks = async (session, models, vars) => {
     models.homeiconcolorblocks = vars.page;
     await session.screen('homeiconcolorblocks');
 };
+exports['Personal Information'] = async (session, models, vars) => {
+    await session.transform.powwowtraining60.action('homeiconcolorblocks', 'Personal Information');
+};
