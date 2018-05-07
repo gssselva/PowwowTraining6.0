f23ff1f9415831d7957b6cd8c265e1a0e6d373b2
Magesh S <mageshbabu.s@flex.com>
2018-05-06 22:32:47 -0700

    [Studio] Add screen "newhome"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,16 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu';
-
+import { newhome_PhonePortrait } from '../pages/newhome/PhonePortrait/newhome';
 export class Screens {
-
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    newhome_PhonePortrait
   ];
-
   static mapping = {
+    'newhome': {
+      PhonePortrait: newhome_PhonePortrait
+    }
   }
-
 }
-
-
