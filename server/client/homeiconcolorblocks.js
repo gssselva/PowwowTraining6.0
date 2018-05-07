d8d1223fc1a1fc75490ce1e5f55741a3e106360d
Selva P <selvabharathi.p@flex.com>
2018-05-07 03:02:29 -0700

    [Webexplorer] Copy screen model "homeiconcolorblocks", Add sourcePath property to screen model "homeiconcolorblocks", Connect screen models (homeiconcolorblocks) to page models

---
+++ server/client/homeiconcolorblocks.js
@@ -0,0 +1,24 @@
+exports.help = async (session, models, vars) => {
+    await session.transform.powwowtraining60.update('homeiconcolorblocks', models.homeiconcolorblocks);
+    await session.transform.powwowtraining60.action('homeiconcolorblocks', 'help');
+};
+exports.alerts = async (session, models, vars) => {
+    await session.transform.powwowtraining60.update('homeiconcolorblocks', models.homeiconcolorblocks);
+    await session.transform.powwowtraining60.action('homeiconcolorblocks', 'alerts');
+};
+exports.newIssue = async (session, models, vars) => {
+    await session.transform.powwowtraining60.update('homeiconcolorblocks', models.homeiconcolorblocks);
+    await session.transform.powwowtraining60.action('homeiconcolorblocks', 'newIssue');
+};
+exports.monitor = async (session, models, vars) => {
+    await session.transform.powwowtraining60.update('homeiconcolorblocks', models.homeiconcolorblocks);
+    await session.transform.powwowtraining60.action('homeiconcolorblocks', 'monitor');
+};
+exports.unassigned = async (session, models, vars) => {
+    await session.transform.powwowtraining60.update('homeiconcolorblocks', models.homeiconcolorblocks);
+    await session.transform.powwowtraining60.action('homeiconcolorblocks', 'unassigned');
+};
+exports.assigned = async (session, models, vars) => {
+    await session.transform.powwowtraining60.update('homeiconcolorblocks', models.homeiconcolorblocks);
+    await session.transform.powwowtraining60.action('homeiconcolorblocks', 'assigned');
+};
