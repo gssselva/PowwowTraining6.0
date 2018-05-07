7bc016f565d7edba4c80514e597d14e262c63ecd
Selva P <selvabharathi.p@flex.com>
2018-05-07 02:56:20 -0700

    [Studio] Add screen "HomeIconColorBlocks"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,14 +2,29 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu';
 import { loginlightblue_PhonePortrait } from '../pages/loginlightblue/PhonePortrait/loginlightblue';
+import { loginpage_PhonePortrait } from '../pages/loginpage/PhonePortrait/loginpage';
+import { loginpage_PhonePortrait } from '../pages/loginpage/PhonePortrait/loginpage';
+import { homeiconcolorblocks_PhonePortrait } from '../pages/homeiconcolorblocks/PhonePortrait/homeiconcolorblocks';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    loginlightblue_PhonePortrait
+    loginlightblue_PhonePortrait,
+    loginpage_PhonePortrait,
+    loginpage_PhonePortrait,
+    homeiconcolorblocks_PhonePortrait
   ];
   static mapping = {
     'loginlightblue': {
       PhonePortrait: loginlightblue_PhonePortrait
+    },
+    'loginpage': {
+      PhonePortrait: loginpage_PhonePortrait
+    },
+    'loginpage': {
+      PhonePortrait: loginpage_PhonePortrait
+    },
+    'homeiconcolorblocks': {
+      PhonePortrait: homeiconcolorblocks_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,4 @@
+export * from './homeiconcolorblocks/index';
 export * from './loginlightblue/index';
 export * from './loginpage/index';
 export * from './menu/index';
